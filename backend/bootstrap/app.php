33818c22a8b4a515e7e02085b9a53359d50143ed
shende-shweta <shweta.shende@globallogic.com>
2026-09-11 11:11:14 +0530

    feat: register ApiKeyMiddleware alias in bootstrap

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
+        $middleware->alias([
+            'api.key' => \App\Http\Middleware\ApiKeyMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
